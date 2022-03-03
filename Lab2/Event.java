f9d1e5a6c27507894bfd3937886eb61d8bbfc52f
Darion01 <dariusgh01@yahoo.com>
2022-03-04 00:20:11 +0200

    Ex2

---
+++ Lab2/Event.java
@@ -0,0 +1,13 @@
+package Lab2;
+
+public class Event {
+
+    private String name;
+    private String type;
+    enum type {lecture_hall,lab}
+
+    private int size;
+    private int start;
+    private int end;
+
+}
